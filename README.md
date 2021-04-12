aa1e2a276c0ee4f57156c4d80a7c34f2ed973bef
Jen Nguyen <jknguyen@mit.edu>
2021-04-11 22:00:38 -0700

    semi automate entry of metadata

--- README.md
+++ README.md
@@ -4,13 +4,17 @@ A collection of scripts analyzing HiPR-FISH imaging data
 
 A work in progress by Jen Nguyen
 
+## organizing experiment meta data
+1. enterMetaData.m
+	- creates and/or builds from data structure called metadata.mat
+
 ## determining single cells from junk and clumps
-1. whos_a_cell.m: threshold determination (by cell width) to distinguish single cells from other particles
+2. whos_a_cell.m: threshold determination (by cell width) to distinguish single cells from other particles
 	- a different section of code for each strain, enables strain specific image processing
 	- these values are important to inform intepretations of signal intensity
 	- for example, clumps have more autofluorescence than single cells
 
 ## measuring signal intensity
-2. intensity_cell_v_bg: plots mean per particle fluor int normalized by background int
+3. intensity_cell_v_bg: plots mean per particle fluor int normalized by background int
 	- does not yet account for single vs clumps of cells
-3. segmentIntensity_GFP: uses whos_a_cell to quantify fluorescence intensity in single cells and clumps
+4. segmentIntensity_GFP: uses whos_a_cell to quantify fluorescence intensity in single cells and clumps
---
+++ enterMetaData.m
@@ -0,0 +1,101 @@
+%% enterMetaData.m
+
+% goal: prompt user for inputs and store to experiment specific structures
+%       stored data:
+%               1. experiment date
+%               2. magnification
+%               3. sample names
+%               4. sample type (pure culture, mixed culture, fecal sample)
+%               5. number of channels
+%               6. growth stage ('lag','exponential','stationary','fecal')
+%
+%       each row is a different experimental "replicate"
+%       each column represents a type of image data that requires the same analysis
+%
+%            column 1 = pure cultures
+%            column 2 = mixed samples
+%            column 3 = fecal samples
+%
+
+
+% last updated: 2021 April 11
+% commit message: first commit, assumes NOMO scope data
+
+
+% OK let's go!
+
+
+%% 0. initialize dimensions of current data structure
+
+clc
+clear
+cd('/Users/jen/such-hipr')
+load('metadata.mat')
+
+
+%% 1. enter meta data for a new experiment
+
+% 1. prompt user for sample type
+prompt = 'Enter experiment type as a string (pure/mixed/fecal): ';
+sampleType = input(prompt);
+newdata(1).sampleType = sampleType;
+
+
+% 2. prompt user for number of channels data
+prompt = 'Enter number of fluorescent channels (i.e. exclude phase): ';
+numFluors = input(prompt);
+newdata(1).numFluors = numFluors;
+
+
+
+% 3. determine column of experiment to add
+if strcmp(sampleType,'pure') == 1 
+    column = 1;                       
+elseif strcmp(sampleType,'mixed') == 1
+    column = 2;
+elseif strcmp(sampleType,'fecal') == 1
+    column = 3;
+else
+    disp('Error: unknown sampleType!');
+end
+
+
+% 4. prompt user for experiment date
+prompt = 'Enter experiment date as a string: ';
+date = input(prompt);
+newdata(1).date = date;
+
+
+% 5. prompt user for growth stage
+prompt = 'Enter growth stage as a string (lag,exponential,stationary,fecal): ';
+growthStage = input(prompt);
+newdata(1).growthStage = growthStage;
+
+
+% 6. prompt user for sample names
+prompt = 'Enter sample names as a cell array of strings ({name1,name2,...}): ';
+samples = input(prompt);
+newdata(1).samples = samples;
+
+
+% 7. prompt user for sample magnification
+prompt = 'Enter sample magnification as an integer (100, 150,...): ';
+magnification = input(prompt);
+newdata(1).magnification = magnification;
+
+
+% 8. assign data structure to new (experiment-specific cell)
+priorEntries = size(metadata);
+if column > priorEntries % new column, row = 1
+    newrow = 1;
+else % column already exists!
+    columndata = metadata(:,column);
+    priorRows = sum(~cellfun(@isempty,columndata));
+    newrow = priorRows + 1;
+end
+metadata{newrow,column} = newdata;
+
+
+%% 8. save storedMetaData
+save('metadata.mat','metadata')
+
