73ebec4e4fcdbc96e6906a64108872d04e685561
Jen Nguyen <jknguyen@mit.edu>
2021-06-07 15:28:42 -0700

    H07 (g6) with kisa in exponential phase

--- whos_a_cell.m
+++ whos_a_cell.m
@@ -22,8 +22,8 @@
 
 % ok, let's go!
 
-% last updated: jen, 2021 April 20
-% commit: L. plantarum in exponential phase
+% last updated: jen, 2021 June 7
+% commit: H07 (g6) with kisa in exponential phase
 
 
 %% 381 - Lactobacillus plantarum ATCC BAA-793 (Firmicute)
@@ -578,6 +578,97 @@ for pp = 1:length(stats)
         color = rgb('HotPink');
         color_text = rgb('DarkMagenta');
     elseif minorAxes(pp) > 1.8 % clump threshold
+        color = rgb('DeepPink');
+        color_text = rgb('DarkMagenta');
+    else
+        color = rgb('DarkTurquoise'); % single cells
+        color_text = rgb('MidnightBlue');
+    end
+
+    figure(3)
+    hold on
+    plot(x_rotated,y_rotated,'Color',color,'lineWidth',lineVal)
+    text((centroid(1)+1)/px_size, (centroid(2)+1)/px_size, num2str(pp),'Color',color_text,'FontSize',10)
+    
+end
+
+%% H07 - S24-7 G6 (exponential phase)
+
+clc
+clear
+
+% 0. initialize experiment meta data
+px_size = 11/150; % 11 um pixels with 150x magnification
+experiment = '2021-06-04';
+data_folder = strcat('/Users/jen/Documents/TropiniLab/Data/Kisa/',experiment);
+cd(data_folder)
+sample = 'G2'; % fixed, no encoding probe
+
+prefix = 'img_';
+suffix = '_position000_time000000000_z000.tif';
+name_phase = strcat(prefix,'channel000',suffix);
+clear prefix suffix
+    
+
+% 0. manually choose one sample image (contains single cells and clumps)
+cd(data_folder)
+sDirectory = dir(strcat(sample,'_*'));
+names = {sDirectory.name};
+img_h07_exp = names{4}; % chosen image
+
+
+% 1. load one 150x phase image
+cd(data_folder)
+cd(strcat(img_h07_exp,'/Default'))
+img_phase = imread(name_phase); % read phase image
+figure(1) % display phase image
+imshow(img_phase, 'DisplayRange',[1000 3000]); %lowering right # increases num sat'd pxls
+title('h07 S24-7 G6 (150x), phase')
+
+
+% 2. create mask using parameters in current data matrix
+%    see segmentIntensity_GFP.m for full comments / annotation
+phase_smoothed = imgaussfilt(img_phase,0.8);
+bw = edge(phase_smoothed,'sobel');
+se = strel('disk',1); % structuring element = disk of radius x pixels
+bw_dil = imdilate(bw, se); % dilate
+bw_fill = imfill(bw_dil, 'holes'); % fill
+bw_final = imerode(imerode(bw_fill,se),se); % erode twice
+figure(2)
+imshow(bw_final)
+title('h07 S24-7 G6 (150x), mask')
+
+
+% 3. measure particle features
+cc = bwconncomp(bw_final);
+stats = regionprops(cc,'Area','Centroid','MajorAxisLength','MinorAxisLength','Eccentricity','Orientation');
+clear bw bw_dil bw_fill se phase_smoothed
+        
+
+% 4. overlay outlines of all tracked particles
+majorAxes = extractfield(stats,'MajorAxisLength')'.*px_size;
+minorAxes = extractfield(stats,'MinorAxisLength')'.*px_size;
+angles = extractfield(stats,'Orientation')';
+
+% 5. vary min and max measurements to determine...
+%      i) below what threshold is background noise?
+%     ii) within what thresholds is a single-cell?
+%    iii) above what threshold is a clump of cells?
+
+figure(3) % this one will get overlaid with particle outlines
+imshow(img_phase, 'DisplayRange',[1000 3000]);
+title('h07 S24-7 G6 (150x), phase')
+
+for pp = 1:length(stats)
+    
+    centroid = stats(pp).Centroid.*px_size;
+    [x_rotated, y_rotated] = drawEllipse2(pp, majorAxes, minorAxes, centroid(1), centroid(2), angles, px_size);
+    lineVal = 1;
+    
+    if minorAxes(pp) < 0.6  % noise threshold
+        color = rgb('HotPink');
+        color_text = rgb('DarkMagenta');
+    elseif minorAxes(pp) > 0.9 % clump threshold
         color = rgb('DeepPink');
         color_text = rgb('DarkMagenta');
     else
