f7e5aa0a68782f8002d4ddddcd2f0eff3b0de34e
kisanaqvi <72049748+kisanaqvi@users.noreply.github.com>
2021-06-16 10:54:06 -0700

    Add files via upload
    Troubleshooting image analysis for FISH Experiment 2 (2021-06-15)

--- whos_a_cell.m
+++ whos_a_cell.m
@@ -433,7 +433,7 @@ clear prefix suffix
 cd(data_folder)
 sDirectory = dir(strcat(sample,'_*'));
 names = {sDirectory.name};
-img_bw25113 = names{10}; % chosen image
+img_bw25113 = names{2}; % chosen image
 
 
 % 1. load one 150x phase image
@@ -694,7 +694,7 @@ px_size = 11/150; % 11 um pixels with 150x magnification
 experiment = '2021-06-04';
 data_folder = strcat('C:/Users/Kisa Naqvi/Documents/TropiniLab/Data/',experiment);
 cd(data_folder)
-sample = 'N3'; % fixed, no encoding probe
+sample = 'N1'; % fixed with NM65 encoding probe
 
 prefix = 'img_';
 suffix = '_position000_time000000000_z000.tif';
@@ -772,6 +772,282 @@ for pp = 1:length(stats)
         color_text = rgb('MidnightBlue');
         %%color = [0 0 1]
         %%color_text = [0 0 1]
+    end
+
+    figure(3)
+    hold on
+    plot(x_rotated,y_rotated,'Color',color,'lineWidth',lineVal)
+    text((centroid(1)+1)/px_size, (centroid(2)+1)/px_size, num2str(pp),'Color',color_text,'FontSize',10)
+    
+end
+
+%% G6 (H07) - S1 2021-06-15 (stationary phase)
+
+
+clc
+clear
+
+% 0. initialize experiment meta data
+px_size = 11/100; % 11 um pixels with 100x magnification
+experiment = '2021-06-15';
+data_folder = strcat('C:/Users/Kisa Naqvi/Documents/TropiniLab/Data/',experiment);
+cd(data_folder)
+sample = 'S1'; % fixed with NM65 encoding probe
+
+prefix = 'img_';
+suffix = '_position000_time000000000_z000.tif';
+name_phase = strcat(prefix,'channel000',suffix);
+clear prefix suffix
+    
+
+% 0. manually choose one sample image (contains single cells and clumps)
+cd(data_folder)
+sDirectory = dir(strcat(sample,'_*'));
+names = {sDirectory.name};
+img_h01_exp = names{10}; % chosen image
+
+
+% 1. load one 100x phase image
+cd(data_folder)
+cd(strcat(img_h01_exp,'/Default'))
+img_phase = imread(name_phase); % read phase image
+figure(1) % display phase image
+imshow(img_phase, 'DisplayRange',[1000 3000]); %lowering right # increases num sat'd pxls
+title('G6 S1 (100x), phase')
+
+
+% 2. create mask using parameters in current data matrix
+%    see segmentIntensity_GFP.m for full comments / annotation
+phase_smoothed = imgaussfilt(img_phase,0.8);
+bw = edge(phase_smoothed,'sobel');
+se = strel('disk',1); % structuring element = disk of radius x pixels
+bw_dil = imdilate(bw, se); % dilate
+bw_fill = imfill(bw_dil, 'holes'); % fill
+bw_final = imerode(imerode(bw_fill,se),se); % erode twice
+figure(2)
+imshow(bw_final)
+title('G6 S1 (100x), mask')
+
+
+% 3. measure particle features
+cc = bwconncomp(bw_final);
+stats = regionprops(cc,'Area','Centroid','MajorAxisLength','MinorAxisLength','Eccentricity','Orientation');
+clear bw bw_dil bw_fill se phase_smoothed
+        
+
+% 4. overlay outlines of all tracked particles
+majorAxes = extractfield(stats,'MajorAxisLength')'.*px_size;
+minorAxes = extractfield(stats,'MinorAxisLength')'.*px_size;
+angles = extractfield(stats,'Orientation')';
+
+% 5. vary min and max measurements to determine...
+%      i) below what threshold is background noise?
+%     ii) within what thresholds is a single-cell?
+%    iii) above what threshold is a clump of cells?
+
+figure(3) % this one will get overlaid with particle outlines
+imshow(img_phase, 'DisplayRange',[1000 3000]);
+title('G6 S1 (100x), phase')
+
+for pp = 1:length(stats)
+    
+    centroid = stats(pp).Centroid.*px_size;
+    [x_rotated, y_rotated] = drawEllipse2(pp, majorAxes, minorAxes, centroid(1), centroid(2), angles, px_size);
+    lineVal = 1;
+    
+    if minorAxes(pp) < 0.6  % noise threshold
+        color = rgb('HotPink');
+        color_text = rgb('DarkMagenta');
+    elseif minorAxes(pp) > 0.9 % clump threshold
+        color = rgb('DeepPink');
+        color_text = rgb('DarkMagenta');
+    else
+        color = rgb('DarkTurquoise'); % single cells
+        color_text = rgb('MidnightBlue');
+    end
+
+    figure(3)
+    hold on
+    plot(x_rotated,y_rotated,'Color',color,'lineWidth',lineVal)
+    text((centroid(1)+1)/px_size, (centroid(2)+1)/px_size, num2str(pp),'Color',color_text,'FontSize',10)
+    
+end
+
+%% G6 (H07) - S2 2021-06-15 (stationary phase)
+
+
+clc
+clear
+
+% 0. initialize experiment meta data
+px_size = 11/100; % 11 um pixels with 100x magnification
+experiment = '2021-06-15';
+data_folder = strcat('C:/Users/Kisa Naqvi/Documents/TropiniLab/Data/',experiment);
+cd(data_folder)
+sample = 'S2'; % fixed with NM65 encoding probe
+
+prefix = 'img_';
+suffix = '_position000_time000000000_z000.tif';
+name_phase = strcat(prefix,'channel000',suffix);
+clear prefix suffix
+    
+
+% 0. manually choose one sample image (contains single cells and clumps)
+cd(data_folder)
+sDirectory = dir(strcat(sample,'_*'));
+names = {sDirectory.name};
+img_h01_exp = names{8}; % chosen image
+
+
+% 1. load one 100x phase image
+cd(data_folder)
+cd(strcat(img_h01_exp,'/Default'))
+img_phase = imread(name_phase); % read phase image
+figure(1) % display phase image
+imshow(img_phase, 'DisplayRange',[1000 3000]); %lowering right # increases num sat'd pxls
+title('G6 S2 (100x), phase')
+
+
+% 2. create mask using parameters in current data matrix
+%    see segmentIntensity_GFP.m for full comments / annotation
+phase_smoothed = imgaussfilt(img_phase,0.8);
+bw = edge(phase_smoothed,'sobel');
+se = strel('disk',1); % structuring element = disk of radius x pixels
+bw_dil = imdilate(bw, se); % dilate
+bw_fill = imfill(bw_dil, 'holes'); % fill
+bw_final = imerode(imerode(bw_fill,se),se); % erode twice
+figure(2)
+imshow(bw_final)
+title('G6 S2 (100x), mask')
+
+
+% 3. measure particle features
+cc = bwconncomp(bw_final);
+stats = regionprops(cc,'Area','Centroid','MajorAxisLength','MinorAxisLength','Eccentricity','Orientation');
+clear bw bw_dil bw_fill se phase_smoothed
+        
+
+% 4. overlay outlines of all tracked particles
+majorAxes = extractfield(stats,'MajorAxisLength')'.*px_size;
+minorAxes = extractfield(stats,'MinorAxisLength')'.*px_size;
+angles = extractfield(stats,'Orientation')';
+
+% 5. vary min and max measurements to determine...
+%      i) below what threshold is background noise?
+%     ii) within what thresholds is a single-cell?
+%    iii) above what threshold is a clump of cells?
+
+figure(3) % this one will get overlaid with particle outlines
+imshow(img_phase, 'DisplayRange',[1000 3000]);
+title('G6 S2 (100x), phase')
+
+for pp = 1:length(stats)
+    
+    centroid = stats(pp).Centroid.*px_size;
+    [x_rotated, y_rotated] = drawEllipse2(pp, majorAxes, minorAxes, centroid(1), centroid(2), angles, px_size);
+    lineVal = 1;
+    
+    if minorAxes(pp) < 0.6  % noise threshold
+        color = rgb('HotPink');
+        color_text = rgb('DarkMagenta');
+    elseif minorAxes(pp) > 0.9 % clump threshold
+        color = rgb('DeepPink');
+        color_text = rgb('DarkMagenta');
+    else
+        color = rgb('DarkTurquoise'); % single cells
+        color_text = rgb('MidnightBlue');
+    end
+
+    figure(3)
+    hold on
+    plot(x_rotated,y_rotated,'Color',color,'lineWidth',lineVal)
+    text((centroid(1)+1)/px_size, (centroid(2)+1)/px_size, num2str(pp),'Color',color_text,'FontSize',10)
+    
+end
+
+%% H03 + H06 - S3 2021-06-15 (stationary phase)
+
+
+clc
+clear
+
+% 0. initialize experiment meta data
+px_size = 11/100; % 11 um pixels with 100x magnification
+experiment = '2021-06-15';
+data_folder = strcat('C:/Users/Kisa Naqvi/Documents/TropiniLab/Data/',experiment);
+cd(data_folder)
+sample = 'S3'; % fixed with NM65 encoding probe
+
+prefix = 'img_';
+suffix = '_position000_time000000000_z000.tif';
+name_phase = strcat(prefix,'channel000',suffix);
+clear prefix suffix
+    
+
+% 0. manually choose one sample image (contains single cells and clumps)
+cd(data_folder)
+sDirectory = dir(strcat(sample,'_*'));
+names = {sDirectory.name};
+img_h01_exp = names{4}; % chosen image
+
+
+% 1. load one 100x phase image
+cd(data_folder)
+cd(strcat(img_h01_exp,'/Default'))
+img_phase = imread(name_phase); % read phase image
+figure(1) % display phase image
+imshow(img_phase, 'DisplayRange',[1000 3000]); %lowering right # increases num sat'd pxls
+title('H03 + H06 (100x), phase')
+
+
+% 2. create mask using parameters in current data matrix
+%    see segmentIntensity_GFP.m for full comments / annotation
+phase_smoothed = imgaussfilt(img_phase,0.8);
+bw = edge(phase_smoothed,'sobel');
+se = strel('disk',1); % structuring element = disk of radius x pixels
+bw_dil = imdilate(bw, se); % dilate
+bw_fill = imfill(bw_dil, 'holes'); % fill
+bw_final = imerode(imerode(bw_fill,se),se); % erode twice
+figure(2)
+imshow(bw_final)
+title('H03 + H06  (100x), mask')
+
+
+% 3. measure particle features
+cc = bwconncomp(bw_final);
+stats = regionprops(cc,'Area','Centroid','MajorAxisLength','MinorAxisLength','Eccentricity','Orientation');
+clear bw bw_dil bw_fill se phase_smoothed
+        
+
+% 4. overlay outlines of all tracked particles
+majorAxes = extractfield(stats,'MajorAxisLength')'.*px_size;
+minorAxes = extractfield(stats,'MinorAxisLength')'.*px_size;
+angles = extractfield(stats,'Orientation')';
+
+% 5. vary min and max measurements to determine...
+%      i) below what threshold is background noise?
+%     ii) within what thresholds is a single-cell?
+%    iii) above what threshold is a clump of cells?
+
+figure(3) % this one will get overlaid with particle outlines
+imshow(img_phase, 'DisplayRange',[1000 3000]);
+title('H03 + H06  (100x), phase')
+
+for pp = 1:length(stats)
+    
+    centroid = stats(pp).Centroid.*px_size;
+    [x_rotated, y_rotated] = drawEllipse2(pp, majorAxes, minorAxes, centroid(1), centroid(2), angles, px_size);
+    lineVal = 1;
+    
+    if minorAxes(pp) < 0.7  % noise threshold
+        color = rgb('HotPink');
+        color_text = rgb('DarkMagenta');
+    elseif minorAxes(pp) > 1.6 % clump threshold
+        color = rgb('DeepPink');
+        color_text = rgb('DarkMagenta');
+    else
+        color = rgb('DarkTurquoise'); % single cells
+        color_text = rgb('MidnightBlue');
     end
 
     figure(3)
